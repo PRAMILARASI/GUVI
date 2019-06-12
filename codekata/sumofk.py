852b5055ef0046786d02de01740fb5781867431a
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-12 14:08:31 +0530

    Update sumofk.py

--- codekata/sumofk.py
+++ codekata/sumofk.py
@@ -1,8 +1,8 @@
-a,b=input().split()
-a=int(a)
-b=int(b)
+p,q=input().split()
+p=int(p)
+q=int(q)
 r=0
 array=[int(x) for x in input().split()]
-for i in range(0,b):
+for i in range(0,q):
   r=r+array[i]
   print(r)
