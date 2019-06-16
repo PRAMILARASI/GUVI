bbb1acfc106cef9517bbfb3c8fbbe249529d27b5
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-16 09:12:54 +0530

    Create prmefactor.py

---
+++ codekata/prmefactor.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(2,n+1):
+    if(n%i==0):
+        c=0
+        for k in range(1,i+1):
+            if(i%k==0):
+                c=c+1
+        if(c==2):
+            print(k,end=" ")
