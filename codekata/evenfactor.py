f8b53edcbb8d942012f87f2acf8d6374bcc32b53
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-19 20:25:22 +0530

    Update evenfactor.py

--- codekata/evenfactor.py
+++ codekata/evenfactor.py
@@ -1,4 +1,5 @@
-A = int(input())
-for i in range(2,a+1,2):
-  if a%i == 0:
-    print(i,end = ' ')
+Ss = int(input())
+for i in range(2,Ss+1):
+    if Ss % i ==0:
+        if i % 2 ==0:
+            print(i,end=' ')
