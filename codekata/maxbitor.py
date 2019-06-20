79f558f10a9d42464d9603c44ce42caf6fc7089b
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-20 20:57:23 +0530

    Update maxbitor.py

--- codekata/maxbitor.py
+++ codekata/maxbitor.py
@@ -1,6 +1,7 @@
-aa2= int(input())
-aa3 = list(map(int,input().split()))
-x=aa3[0]
-for i in range(1,len(aa3)):
-x= x | aa3[i]
-print(x)
+na=int(input())
+l=list(map(int,input().split()))
+c=l[0]
+for i in range(1,len(l)):
+    c=c|l[i]
+print(c)
+#maxma
