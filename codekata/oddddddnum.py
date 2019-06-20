9a0c406bc01d2c550d8ef4816a8815cf8ff179b2
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-20 21:47:49 +0530

    Update oddddddnum.py

--- codekata/oddddddnum.py
+++ codekata/oddddddnum.py
@@ -1,7 +1,7 @@
-a3 = list(map(int,input().split()))
+aa3 = list(map(int,input().split()))
 aa = list(map(int,input().split()))
 l=[]
 for i in range(0,len(aa)):
     if aa[i]%2 !=0:
         l.append(aa[i])
-print(l[a3[1]-1])
+print(l[aa3[1]-1])
