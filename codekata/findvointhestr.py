ab5c4d990e77594748b474481936ed4193a6825f
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-21 18:32:37 +0530

    Create findvointhestr.py

---
+++ codekata/findvointhestr.py
@@ -0,0 +1,8 @@
+nk1=int(input())
+l=[]
+for i in range(nk1):
+    l.append(input())
+if all("a" in i or "i" in i or "e" in i or "o" in i or "u" in i for i in l):
+    print("yes")
+else:
+    print("no")
