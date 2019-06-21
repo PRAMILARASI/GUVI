acd9a1fa00bcb10f42b3b4c560f6d05c2738c81c
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-21 18:16:49 +0530

    Update sortless.py

--- codekata/sortless.py
+++ codekata/sortless.py
@@ -1,8 +1,15 @@
-kk,lk=map(int,input().split())
-a=list(map(int,input().split()))
-b=[]
-for i in a:
-    if a.count(i)<l and i not in b:
-        b.append(i)
-c=sorted(b)
-print(*c)
+aax=list(map(int,input().split()))
+a=input().split()
+z=[]
+k=aax[1]
+n=aax[0]
+for i in range(0,n):
+    c=0
+    for j in range(0,n):
+        if a[i]==a[j]:
+            c=c+1
+    if c<k:
+        z.append(a[i])
+z=list(dict.fromkeys(z))
+z=sorted(z)
+print(*z)
