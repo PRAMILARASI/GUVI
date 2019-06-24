d779c82178505e70a08eaed64643a041651856ad
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-24 18:56:07 +0530

    Create comple.py

---
+++ codekata/comple.py
@@ -0,0 +1,19 @@
+sa1=input()
+
+t=input()
+
+a=sa1+t
+
+c=1
+
+for i in a:
+
+	if a.count(i)==1:
+
+		c=1
+
+	else:
+
+		c=0
+
+print("complementary" if ((c==1) and (len(a)==26)) else "non-complementary")
