4da93e1f33e67546728b4ad035d4ed3be6525f49
PRAMILARASI <51673218+PRAMILARASI@users.noreply.github.com>
2019-06-24 19:25:52 +0530

    Create lencoprime.py

---
+++ codekata/lencoprime.py
@@ -0,0 +1,23 @@
+as,s2=map(str,input().split())
+
+x=len(as)
+
+y=len(s2)
+
+n=max(x,y)
+
+c=0
+
+for i in range(1,n):
+
+    if x%i==0 and y%i==0:
+
+        c=c+1
+
+if c==1:
+
+    print("yes")
+
+else:
+
+    print("no")
